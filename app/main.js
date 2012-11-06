b307045fcf24f3148e28e0a7bd6937e4f7d55375
Brian Cavalier <brian@hovercraftstudios.com>
2012-11-05 23:14:08 -0500

    Move theme, use moduleLoader for theme css loading

--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
 define({
 
-	theme: { module: 'css!theme.css' },
+	// Load a basic theme. This is just a CSS file, and since a moduleLoader is
+	// configured in run.js, curl knows to load this as CSS.
+	theme: { module: 'theme/basic.css' },
 
+	// Create a simple view by rendering html, replacing some i18n strings
+	// and loading CSS.  Then, insert into the DOM
 	message: {
 		render: {
 			template: { module: 'text!welcome/template.html' },
@@ -11,6 +15,7 @@ define({
 		insert: { at: 'dom.first!body' }
 	},
 
+	// Wire.js plugins
 	plugins: [
 		{ module: 'wire/dom', classes: { init: 'loading' } },
 		{ module: 'wire/dom/render' }
--- app/run.js
+++ app/run.js
@@ -1,19 +1,28 @@
 (function(curl) {
 
 	var config = {
-		baseUrl: 'app',
-		pluginPath: 'curl/plugin',
+		// baseUrl: '',
 		//paths: {
 			// Configure paths here
 		//},
 		packages: [
-			// Add your packages here
-			{ name: 'curl', location: '../lib/curl/src/curl' },
-			{ name: 'wire', location: '../lib/wire', main: 'wire' },
-			{ name: 'cola', location: '../lib/cola', main: 'cola' },
-			{ name: 'when', location: '../lib/when', main: 'when' },
-			{ name: 'meld', location: '../lib/meld', main: 'meld' },
-			{ name: 'poly', location: '../lib/poly' }
+			// Define application-level packages
+			{ name: 'welcome', location: 'app/welcome' },
+
+			// Define a theme package, and configure it to always use the css module loader
+			// No need to use AMD 'css!' plugin to load things in this package, it will happen
+			// automatigally.
+			// WARNING: The moduleLoader config syntax will be changing in an upcoming version
+			// of curl.
+			{ name: 'theme',   location: 'theme', config: { moduleLoader: 'curl/plugin/css' } },
+
+			// Add third-party packages here
+			{ name: 'curl', location: 'lib/curl/src/curl' },
+			{ name: 'wire', location: 'lib/wire', main: 'wire' },
+			{ name: 'cola', location: 'lib/cola', main: 'cola' },
+			{ name: 'when', location: 'lib/when', main: 'when' },
+			{ name: 'meld', location: 'lib/meld', main: 'meld' },
+			{ name: 'poly', location: 'lib/poly' }
 		],
 		// Polyfill everything ES5-ish
 		preloads: ['poly/all']
@@ -21,6 +30,6 @@
 		//preloads: ['poly/array', 'poly/function', 'poly/json', 'poly/object', 'poly/string', 'poly/xhr']
 	};
 
-	curl(config, ['wire!main']);
+	curl(config, ['wire!app/main']);
 
 })(curl);
