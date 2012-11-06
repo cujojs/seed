81d19d8e0865841b043bba9d4668a7e62e06a40f
Brian Cavalier <brian@hovercraftstudios.com>
2012-11-05 23:18:32 -0500

    Fix whitespace

--- app/run.js
+++ app/run.js
@@ -14,7 +14,7 @@
 			// automatigally.
 			// WARNING: The moduleLoader config syntax will be changing in an upcoming version
 			// of curl.
-			{ name: 'theme',   location: 'theme', config: { moduleLoader: 'curl/plugin/css' } },
+			{ name: 'theme', location: 'theme', config: { moduleLoader: 'curl/plugin/css' } },
 
 			// Add third-party packages here
 			{ name: 'curl', location: 'lib/curl/src/curl' },
