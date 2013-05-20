77f0f37c7b52b60c341ba48c3eb0deb8ed713900
unscriptable <john@unscriptable.com>
2013-05-20 11:43:43 -0400

    add example success and error handlers

--- app/run.js
+++ app/run.js
@@ -30,6 +30,47 @@
 		//preloads: ['poly/array', 'poly/function', 'poly/json', 'poly/object', 'poly/string', 'poly/xhr']
 	};
 
-	curl(config, ['wire!app/main']);
+	curl(config, ['wire!app/main']).then(success, fail);
+
+	// Success! curl.js indicates that your app loaded successfully!
+	function success () {
+		var msg;
+		// When using wire, the success callback is typically not needed since
+		// wire will compose and initialize the app from the main spec.
+		// However, this callback can be useful for executing startup tasks
+		// you don't want to put into a wire spec, such as this:
+		msg = 'Note! 404 errors for locale-specific i18n files are normal '
+			+ 'during development and can be ignored. '
+			+ 'These errors do not appear in internationalized apps that '
+			+ 'have been bundled for production.';
+		console.log(msg);
+	}
+
+	// Oops. curl.js indicates that your app failed to load correctly.
+	function fail (ex) {
+		var el, msg;
+		// There are many ways to handle errors. This is just a simple example.
+		// Note: you cannot rely on any specific library or shim to be
+		// loaded at this point.  Therefore, you must use standard DOM
+		// manipulation and legacy IE equivalents.
+		console.log('an error happened during loading :\'(');
+		console.log(ex.message);
+		if (ex.stack) console.log(ex.stack);
+		el = document.getElementById('errout');
+		msg = 'An error occurred while loading: '
+			+ ex.message
+			+ '. See the console for more information.';
+		if (el) {
+			// inject the error message
+			if ('textContent' in el) el.textContent = msg;
+			else el.innerText = msg;
+			// clear styling that may be hiding the error message
+			el.style.display = '';
+			document.documentElement.className = '';
+		}
+		else {
+			throw msg;
+		}
+	}
 
 })(curl);
