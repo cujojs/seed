a5a22bee06ae03205adc10369c580324b0645fff
Scott Andrews <scothis@gmail.com>
2013-08-19 17:11:13 -0400

    add msgs.js to seed

--- app/run.js
+++ app/run.js
@@ -22,6 +22,8 @@
 			{ name: 'curl', location: 'lib/curl/src/curl' },
 			{ name: 'wire', location: 'lib/wire', main: 'wire' },
 			{ name: 'cola', location: 'lib/cola', main: 'cola' },
+			{ name: 'rest', location: 'lib/rest', main: 'rest' },
+			{ name: 'msgs', location: 'lib/msgs', main: 'msgs' },
 			{ name: 'when', location: 'lib/when', main: 'when' },
 			{ name: 'meld', location: 'lib/meld', main: 'meld' },
 			{ name: 'poly', location: 'lib/poly' }
